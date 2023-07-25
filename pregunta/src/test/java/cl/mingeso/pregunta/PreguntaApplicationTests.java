162fb066cc8f4aec4c73e14f949e92043721a9da
Gonzalo Ordenes <gonzalo.ordenes@usach.cl>
2023-07-25 04:13:35 -0400

    first commit

---
+++ pregunta/src/test/java/cl/mingeso/pregunta/PreguntaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mingeso.pregunta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
